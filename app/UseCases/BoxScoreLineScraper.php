fb273b96b531d37b9d3bb1e8ac096dc3a3c9633d
Dee Barizo <deebarizo@gmail.com>
2016-10-30 07:40:08 +0000

    Works on box score line scraper

--- app/UseCases/BoxScoreLineScraper.php
+++ app/UseCases/BoxScoreLineScraper.php
@@ -38,6 +38,8 @@ class BoxScoreLineScraper {
 			$teams[$i]['br_name_lowercase'] = strtolower($team);
 		}
 
+		$boxScoreLines;
+
 		foreach ($teams as $team) {
 			
 			$numOfTableRows = $crawler->filter('div#all_box_'.$team['br_name_lowercase'].'_basic')->filter('tbody')->filter('tr')->count();
@@ -63,8 +65,88 @@ class BoxScoreLineScraper {
 					$boxScoreLine['fg'] = $playerRow->filter('td')->eq(1)->text();
 					$boxScoreLine['fga'] = $playerRow->filter('td')->eq(2)->text();
 					$boxScoreLine['fg_percentage'] = $playerRow->filter('td')->eq(3)->text();
+					$boxScoreLine['3p'] = intval($playerRow->filter('td')->eq(4)->text());
+					$boxScoreLine['3pa'] = $playerRow->filter('td')->eq(5)->text();
+					$boxScoreLine['3p_percentage'] = $playerRow->filter('td')->eq(6)->text();
+					$boxScoreLine['ft'] = $playerRow->filter('td')->eq(7)->text();
+					$boxScoreLine['fta'] = $playerRow->filter('td')->eq(8)->text();
+					$boxScoreLine['ft_percentage'] = $playerRow->filter('td')->eq(9)->text();
+					$boxScoreLine['orb'] = $playerRow->filter('td')->eq(10)->text();
+					$boxScoreLine['drb'] = $playerRow->filter('td')->eq(11)->text();
+					$boxScoreLine['trb'] = intval($playerRow->filter('td')->eq(12)->text());
+					$boxScoreLine['ast'] = intval($playerRow->filter('td')->eq(13)->text());
+					$boxScoreLine['stl'] = intval($playerRow->filter('td')->eq(14)->text());
+					$boxScoreLine['blk'] = intval($playerRow->filter('td')->eq(15)->text());
+					$boxScoreLine['tov'] = intval($playerRow->filter('td')->eq(16)->text());
+					$boxScoreLine['pf'] = $playerRow->filter('td')->eq(17)->text();
+					$boxScoreLine['pts'] = intval($playerRow->filter('td')->eq(18)->text());
 
-					ddAll($boxScoreLine);
+					$doubleDigitCount = 0;
+
+					if ($boxScoreLine['pts'] >= 10) {
+
+						$doubleDigitCount++;
+					}
+
+					if ($boxScoreLine['trb'] >= 10) {
+
+						$doubleDigitCount++;
+					}
+
+					if ($boxScoreLine['ast'] >= 10) {
+
+						$doubleDigitCount++;
+					}
+
+					if ($boxScoreLine['blk'] >= 10) {
+
+						$doubleDigitCount++;
+					}
+
+					if ($boxScoreLine['stl'] >= 10) {
+
+						$doubleDigitCount++;
+					}
+
+					$bonusPoints = 0;
+
+					if ($doubleDigitCount >= 2) {
+
+						$bonusPoints += 1.5; // double-double bonus
+					}
+
+					if ($doubleDigitCount >= 3) {
+
+						$bonusPoints += 3; // triple-double bonus
+					}
+
+					$boxScoreLine['dk_pts'] = $boxScoreLine['pts'] + 
+												($boxScoreLine['3p'] * 0.5) + 
+												($boxScoreLine['trb'] * 1.25) + 
+												($boxScoreLine['ast'] * 1.5) + 
+												($boxScoreLine['stl'] * 2) + 
+												($boxScoreLine['blk'] * 2) + 
+												($boxScoreLine['tov'] * -0.5) + 
+												$bonusPoints;
+
+					$playerRow = $crawler->filter('div#all_box_'.$team['br_name_lowercase'].'_advanced')->filter('tbody')->filter('tr')->eq($i);
+
+					$boxScoreLine['ts_percentage'] = $playerRow->filter('td')->eq(1)->text();
+					$boxScoreLine['efg_percentage'] = $playerRow->filter('td')->eq(2)->text();
+					$boxScoreLine['3pa_rate'] = $playerRow->filter('td')->eq(3)->text();
+					$boxScoreLine['fta_rate'] = $playerRow->filter('td')->eq(4)->text();
+					$boxScoreLine['orb_percentage'] = $playerRow->filter('td')->eq(5)->text();
+					$boxScoreLine['drb_percentage'] = $playerRow->filter('td')->eq(6)->text();
+					$boxScoreLine['trb_percentage'] = $playerRow->filter('td')->eq(7)->text();
+					$boxScoreLine['ast_percentage'] = $playerRow->filter('td')->eq(8)->text();
+					$boxScoreLine['stl_percentage'] = $playerRow->filter('td')->eq(9)->text();
+					$boxScoreLine['blk_percentage'] = $playerRow->filter('td')->eq(10)->text();
+					$boxScoreLine['tov_percentage'] = $playerRow->filter('td')->eq(11)->text();
+					$boxScoreLine['usg_percentage'] = $playerRow->filter('td')->eq(12)->text();
+
+					$boxScoreLines[] = $boxScoreLine;
+
+					prf($boxScoreLine);
 				} 
 			}
 		}
