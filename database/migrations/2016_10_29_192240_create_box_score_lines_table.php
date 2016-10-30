95af34f6a0a7a754e0e4b50dbd855980797c5830
Dee Barizo <deebarizo@gmail.com>
2016-10-30 00:49:54 +0000

    Creates migration for box_score_lines table

---
+++ database/migrations/2016_10_29_192240_create_box_score_lines_table.php
@@ -0,0 +1,69 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBoxScoreLinesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('box_score_lines', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('game_id')->unsigned();
+            $table->foreign('game_id')->references('id')->on('games');
+            $table->integer('team_id')->unsigned();
+            $table->foreign('team_id')->references('id')->on('teams');
+            $table->integer('player_id')->unsigned();
+            $table->foreign('player_id')->references('id')->on('players');
+            $table->decimal('mp', 5, 2);
+            $table->integer('fg');
+            $table->integer('fga');
+            $table->decimal('fg_percentage', 5, 2)->nullable(); // if they take no shots
+            $table->integer('3p');
+            $table->integer('3pa');
+            $table->decimal('3p_percentage', 5, 2)->nullable();
+            $table->integer('ft');
+            $table->integer('fta');
+            $table->decimal('ft_percentage', 5, 2)->nullable();
+            $table->integer('orb');
+            $table->integer('drb');
+            $table->integer('trb');
+            $table->integer('ast');
+            $table->integer('stl');
+            $table->integer('blk');
+            $table->integer('tov');
+            $table->integer('pf');
+            $table->integer('pts');
+            $table->integer('dk_pts');
+            $table->decimal('ts_percentage', 5, 2)->nullable();
+            $table->decimal('efg_percentage', 5, 2)->nullable();
+            $table->decimal('3pa_rate', 5, 2)->nullable();
+            $table->decimal('fta_rate', 5, 2)->nullable();
+            $table->decimal('orb_percentage', 5, 2);
+            $table->decimal('drb_percentage', 5, 2);
+            $table->decimal('trb_percentage', 5, 2);
+            $table->decimal('ast_percentage', 5, 2);
+            $table->decimal('stl_percentage', 5, 2);
+            $table->decimal('blk_percentage', 5, 2);
+            $table->decimal('tov_percentage', 5, 2)->nullable();
+            $table->decimal('usg_percentage', 5, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('box_score_lines');
+    }
+}
