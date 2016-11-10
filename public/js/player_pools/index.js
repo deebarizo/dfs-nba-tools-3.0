ba493031215438cbb8081baadc11678ca988061c
Dee Barizo <deebarizo@gmail.com>
2016-11-10 19:12:51 +0000

    Adds salary filter

--- public/js/player_pools/index.js
+++ public/js/player_pools/index.js
@@ -45,6 +45,67 @@ $(document).ready(function() {
 
 
 	/****************************************************************************************
+	SALARY FILTER
+	****************************************************************************************/
+
+	// https://www.datatables.net/examples/plug-ins/range_filtering.html
+
+	$.fn.dataTable.ext.search.push(
+
+	    function(settings, data, dataIndex) {
+	        
+	        var targetSalary = parseInt($('.salary-input').val(), 10);
+
+	        var modifier = $('input:radio[name=salary-toggle]:checked').val();
+
+	        var salary = parseFloat(data[5]) || 0; // use data for the salary column
+
+	        if (modifier === 'greater-than') {
+
+	        	if (salary >= targetSalary) {
+
+	        		return true;
+	        	
+	        	} else {
+
+	        		return false;
+	        	}
+	        }
+
+	        if (modifier === 'less-than') {
+
+	        	if (salary <= targetSalary) {
+
+	        		return true;
+	        	
+	        	} else {
+
+	        		return false;
+	        	}
+	        }		 
+	    }
+	);
+
+	$('.salary-input').keyup(function() {
+
+		playerPoolTable.draw();
+	});
+
+	$("input[name=salary-toggle]:radio").change(function() {
+
+		playerPoolTable.draw();
+	});
+
+
+	$('.salary-reset').on('click', function(event) { 
+		$('.salary-input').val(100000);
+		$('#less-than').prop('checked', true);
+
+		playerPoolTable.draw();
+	});
+
+
+	/****************************************************************************************
 	POSITION FILTER
 	****************************************************************************************/
 
