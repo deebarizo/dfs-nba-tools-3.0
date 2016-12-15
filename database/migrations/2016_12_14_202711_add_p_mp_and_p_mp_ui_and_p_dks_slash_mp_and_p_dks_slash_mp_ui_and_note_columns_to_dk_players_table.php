a266e4eb7137b279271eed63c3c7b89f3841e3d9
Dee Barizo <deebarizo@gmail.com>
2016-12-15 02:32:07 +0000

    Adds columns to dk_players including p_mp and p_mp_ui

---
+++ database/migrations/2016_12_14_202711_add_p_mp_and_p_mp_ui_and_p_dks_slash_mp_and_p_dks_slash_mp_ui_and_note_columns_to_dk_players_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddPMpAndPMpUiAndPDksSlashMpAndPDksSlashMpUiAndNoteColumnsToDkPlayersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('dk_players', function ($table) {
+            
+            $table->decimal('p_mp', 4, 2)->nullable()->after('ownership_percentage');
+            $table->string('p_mp_ui')->nullable()->after('p_mp'); // 'ui' stands for 'update instructions'
+            $table->decimal('p_dks_slash_mp', 4, 2)->nullable()->after('p_mp_ui');
+            $table->string('p_dks_slash_mp_ui')->nullable()->after('p_dks_slash_mp');
+            $table->string('note')->nullable()->after('p_dks_slash_mp_ui');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('dk_players', function ($table) {
+
+            $table->dropColumn('p_mp');
+            $table->dropColumn('p_mp_ui');
+            $table->dropColumn('p_dks_slash_mp');
+            $table->dropColumn('p_dks_slash_mp_ui');
+            $table->dropColumn('note');
+        });
+    }
+}
